63349eb241b3425db6d202b7af4603405c5d302b
mystyleplatform <mystyleplatform@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-27 02:30:38 +0000

    Basic PHPUnit coverage now complete.
    git-svn-id: https://plugins.svn.wordpress.org/mystyle-custom-product-designer@1188422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/tests/includes/admin/test-mystyle-woocommerce-admin-product.php
+++ trunk/tests/includes/admin/test-mystyle-woocommerce-admin-product.php
@@ -64,8 +64,12 @@ class MyStyleWooCommerceAdminProductTest extends WP_UnitTestCase {
      * Test the add_mystyle_data_panel function
      */    
     public function test_add_mystyle_data_panel() {
-        //TODO: Need to be able to set or mock the global $post variable
-        /*
+        global $post;
+        require_once( MYSTYLE_PATH . '../woocommerce/includes/admin/wc-meta-box-functions.php' );
+        
+        $post = new stdClass();
+        $post->ID = 1;
+        
         $mystyle_wc_admin_product = new MyStyle_WooCommerce_Admin_Product();
         
         //Assert that the data panel was rendered
@@ -74,14 +78,48 @@ class MyStyleWooCommerceAdminProductTest extends WP_UnitTestCase {
         $outbound = ob_get_contents();
         ob_end_clean();
         $this->assertContains( '<div id="mystyle_product_data" class="panel woocommerce_options_panel">', $outbound );
-         */
+         
     }
     
     /**
      * Test the process_mystyle_data_panel function
      */   
-    public function process_mystyle_data_panel() {
-        //TODO
+    public function test_process_mystyle_data_panel() {
+        global $post;
+        
+        $post_id = 1;
+        
+        //Mock the global $post var and $_POST
+        $post = new stdClass();
+        $post->ID = $post_id;
+        
+        $_POST = array();
+        $_POST['_mystyle_enabled'] = true;
+        $_POST['_mystyle_template_id'] = 1;
+        
+        // Create post object
+        $test_post = array(
+            'ID'            => $post_id,
+            'post_title'    => 'Test post',
+            'post_content'  => 'This is a test post.',
+            'post_status'   => 'publish',
+            'post_author'   => 1,
+            'post_category' => array(8,39)
+        );
+
+        // Insert the post into the database
+        wp_insert_post( $test_post );
+        
+        //Call the function
+        MyStyle_WooCommerce_Admin_Product::process_mystyle_data_panel( $post_id );
+        
+        //Get the post meta
+        $mystyle_enabled = get_post_meta( $post->ID, '_mystyle_enabled', true );
+        $template_id = get_post_meta( $post->ID, '_mystyle_template_id', true );
+       
+        //Assert that the post meta was set
+        $this->assertEquals( 'yes', $mystyle_enabled );
+        $this->assertEquals( 1, $template_id );
     }
     
 }
