698413d9051f3e24cca7990d972a41e6bb146fb9
mystyleplatform <mystyleplatform@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-02 00:00:04 +0000

    Added mobile support, admin UI enhancements.
    git-svn-id: https://plugins.svn.wordpress.org/mystyle-custom-product-designer/trunk@1190991 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcodes/class-mystyle-customizer-shortcode.php
+++ includes/shortcodes/class-mystyle-customizer-shortcode.php
@@ -31,22 +31,27 @@ abstract class MyStyle_Customizer_Shortcode {
         //}
         $encoded_settings = base64_encode( json_encode( $settings ) );
         
-        $mystyle_url = "http://customizer.ogmystyle.com/" .
+        $customizer_query_string =
                         "?app_id=$mystyle_app_id" . 
                         "&amp;product_id=$mystyle_template_id" . 
                         "&amp;settings=$encoded_settings" . 
                         "&amp;passthru=local_product_id,$product_id";
         
-        $out = '<iframe ' .
-                    'id="customizer-iframe" ' .
-                    'frameborder="0" '.
-                    'hspace="0" ' .
-                    'vspace="0" ' .
-                    'scrolling="no" ' .
-                    'src="' . $mystyle_url . '" ' .
-                    'width="950" ' .
-                    'height="550" ' .
-                '></iframe>';
+        //---------- variables for use by the view layer ---------
+        $customizer_url = 'http://customizer.ogmystyle.com/' . $customizer_query_string;
+        $mobile_customizer_url = 'http://customizer-js.ogmystyle.com/' . $customizer_query_string;
+        
+        $force_mobile = 1;
+        if ( isset( $_GET['force_mobile'] ) ) {
+            $force_mobile = 1;
+        }
+        
+        // ---------- Call the view layer ------- //
+        ob_start();
+        require( MYSTYLE_TEMPLATES . 'customizer.php' );
+        $out = ob_get_contents();
+        ob_end_clean();
+        // -------------------------------------- //
                     
         if( ( defined( 'MYSTYLE_ENABLE_MOCK_SUBMIT_BUTTON' ) ) && ( MYSTYLE_ENABLE_MOCK_SUBMIT_BUTTON == true ) ) {
             //Add the mock form (for testing)
