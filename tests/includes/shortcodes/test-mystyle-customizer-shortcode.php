32c1ed759157b033af0b4d66bb4a463074441945
mystyleplatform <mystyleplatform@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 22:47:55 +0000

    Added listing of customizable products to the Customize page.
    git-svn-id: https://plugins.svn.wordpress.org/mystyle-custom-product-designer/trunk@1210026 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/includes/shortcodes/test-mystyle-customizer-shortcode.php
+++ tests/includes/shortcodes/test-mystyle-customizer-shortcode.php
@@ -10,6 +10,21 @@
 class MyStyleCustomizerShortcodeTest extends WP_UnitTestCase {
     
     /**
+     * Test the modify_woocommerce_shortcode_products_query function with valid parameters.
+     */    
+    public function test_modify_woocommerce_shortcode_products_query() {
+        
+        //Mock the args
+        $args = array();
+        $args['meta_query'] = array();
+        
+        $modified_args = MyStyle_Customizer_Shortcode::modify_woocommerce_shortcode_products_query( $args );
+        
+        //assert that the modified args include the mystyle_enabled meta key
+        $this->assertContains( '_mystyle_enabled', $modified_args['meta_query'][0]['key'] );
+    }
+    
+    /**
      * Test the output function with valid parameters.
      */    
     public function test_output_with_valid_params() {
@@ -24,12 +39,12 @@ class MyStyleCustomizerShortcodeTest extends WP_UnitTestCase {
     /**
      * Test the output function with invalid/no params.
      */    
-    public function test_output_with_invalid_params() {
+    public function test_output_with_no_params() {
         
         $output = MyStyle_Customizer_Shortcode::output();
         
         //assert that the output includes an iframe tag
-        $this->assertContains( 'You\'ll need to select a product to customize first!', $output );
+        $this->assertContains( 'Sorry, no products are currently available for customization.', $output );
     }
     
 }
